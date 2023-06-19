c2001855ad1ee1a2c7da6f923d563a68de3d82d7
Aditya Revat <adi.revat@cmrsurgical.com>
2023-06-19 16:51:41 +0100

    initial commit of python

---
+++ main.py
@@ -0,0 +1,16 @@
+print("Hello world!")
+
+#Factors:
+#Sea or freshwater
+#Which body of water (perhaps an autofill option)
+#wetsuit/drysuit
+#thickness of exposure suit
+#Air tank size
+#"end point": record weight used
+
+#maybe data:
+#temperature
+
+#for calculating:
+#recommend a weight
+#show data from similar dives
